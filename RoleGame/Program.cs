fe0dcc3ef7e61873db6e89ccf81095fd70386946
GlobusOffZeWorld <gleb737.gl@gmail.com>
2021-04-18 18:20:12 +0300

    make addHealthSpell and add castSpell

--- RoleGame/Program.cs
+++ RoleGame/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using RoleGame.Classes;
+using RoleGame.Classes.Spells;
 
-namespace RoleGame
-{
-    class Program
-    {
-        static void Main(string[] args) {
-            //
-        }
+namespace RoleGame {
+  class Program {
+    static void Main(string[] args) {
+      // AddHealthSpell spell = new AddHealthSpell();
+      //
+      // PlayerWithMagic playerWithMagic =
+      //   new PlayerWithMagic("Gena", PlayerParams.Race.Elf, PlayerParams.Sex.Female, 19);
+      // Player player = new Player("Lexa", PlayerParams.Race.Orc, PlayerParams.Sex.Male, 25);
+      //
+      // Console.WriteLine(playerWithMagic.ToString());
+      // Console.WriteLine(player.ToString());
+      //
+      // player.Health = 90;
+      //
+      // Console.WriteLine(player.ToString());
+      //
+      // playerWithMagic.CastSpell(spell.UseSpell, player, 20);
+      //
+      // Console.WriteLine(playerWithMagic.ToString());
+      // Console.WriteLine(player.ToString());
     }
+  }
 }
