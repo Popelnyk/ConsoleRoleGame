4fcfca9fecbfc32d97f57cbc1000b87d476d63a2
GlobusOffZeWorld <gleb737.gl@gmail.com>
2021-05-01 00:40:13 +0300

    make inventory

---
+++ RoleGame/Classes/Inventory/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using RoleGame.AbstractClasses;
+
+namespace RoleGame.Classes.Inventory {
+  public class Inventory {
+
+    public Inventory() {
+      _bag = new List<AbstractArtifact>();
+    }
+
+    public void PickUpArtifact(AbstractArtifact artifact) {
+      Bag.Add(artifact);
+    }
+
+    public void ThrowAwayArtifact(AbstractArtifact artifact) {
+      if (Bag.Contains(artifact)) {
+        Bag.Remove(artifact);
+      } else {
+        //message
+      }
+    }
+
+    public void TransferArtifactToAnotherPlayer(AbstractArtifact artifact, Player playerReceiver) {
+      ThrowAwayArtifact(artifact);
+      playerReceiver.Inventory.PickUpArtifact(artifact);
+    }
+
+    public bool ContainsArtifact(AbstractArtifact artifact) {
+      return Bag.Contains(artifact);
+    }
+
+    public List<AbstractArtifact> Bag {
+      get => _bag;
+      private set => _bag = value;
+    }
+    
+    
+    private List<AbstractArtifact> _bag;
+  }
+}
